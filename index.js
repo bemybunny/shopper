c64a0b79f4bdba52f901340bb1e818a327e5e761
bemybunny <bemybunny123@gmail.com>
2024-04-19 15:43:53 +0530

    done changes

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.post('/addtocart', fetchUser, async (req, res) => {
 app.post('/removefromcart',fetchUser ,async(req,res)=>{
     console.log("removed",req.body.itemId)
     let userData = await Users.findOne({_id:req.user.id});
-    if(UserData.cartData[req.body.itemId]>0)
+    if(userData.cartData[req.body.itemId]>0)
     userData.cartData[req.body.itemId]-=1;
     await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
     res.send("Deleted")
