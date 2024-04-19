cf437234f9359abee341245e8cf6b4050d3df4d9
bemybunny <bemybunny123@gmail.com>
2024-04-19 14:58:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,152 @@
+const port = process.env.PORT||5000;  
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+require('dotenv').config();
+app.use(express.json());
+app.use(cors('*'));
+
+//Database Connection with MONGODB
+mongoose.connect(process.env.DATABASEURL)
+
+//API Creation
+app.get('/',(req,res)=>{
+    res.send('Express is running')
+})
+
+//shema for creating User model
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+app.post('/signup',async (req,res) => {
+    try{
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({
+            success:false,errors:"existign user found with same email"
+        })
+    }
+
+    let cart = {};
+    for(let i=0;i<300;i++){
+        cart[i]=0;
+    }
+
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+ 
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({
+        success:true,
+        token
+    })}
+    catch(error){
+        console.log("Error is: "+ error);
+    }
+})
+
+//creating endpoint for user login
+
+app.post('/login',async(req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare = req.body.password === user.password; 
+        if(passCompare){
+            const data={
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_ecom');
+            res.json({success:true,token});
+        }
+        else{
+            res.json({success:false,errors:"Wrong Password"});
+        }
+    }
+    else{
+        res.json({success:false,errors:"Wrong email-id"})
+    }
+})
+
+const fetchUser = async(req,res,next)=>{
+    const token = req.header('auth-token');
+    if(!token){
+        res.status(401).send({errors:"please authenticate using valid tokens"})
+    }else{
+        try{
+            const data=jwt.verify(token,'secret_ecom');
+            req.user = data.user;
+            next();
+        }catch(error){
+            res.status(401).send({errors:"please authenticate using a valid token"})
+        } 
+    }
+}
+
+//creating endpoint for adding products in cartdata
+
+app.post('/addtocart', fetchUser, async (req, res) => {
+    let userData = await Users.findOne({ _id: req.user.id });
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+    res.json({ message: "Added" }); // Sending JSON response
+});
+
+
+//creating endpoint to remove the products from cartdata
+app.post('/removefromcart',fetchUser ,async(req,res)=>{
+    console.log("removed",req.body.itemId)
+    let userData = await Users.findOne({_id:req.user.id});
+    if(UserData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId]-=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Deleted")
+})
+
+app.post('/getcart',fetchUser,async(req,res)=>{
+    console.log("GetCart");
+    let userData= await Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log('Server Running on Port'+ port);
+    }else{
+        console.log("Error is: "+ error);
+    }
+})
